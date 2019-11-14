288712228e77bf0e95987657b0961d2db9f29ca2
is108 <41418671+is108@users.noreply.github.com>
2019-11-15 03:29:08 +0700

    Update views.py

--- laba/firstapp/views.py
+++ laba/firstapp/views.py
@@ -3,9 +3,9 @@ from django.http import HttpResponse
 from os import listdir, mkdir, rmdir
 from os.path import isfile, join, isdir, dirname, realpath
 import requests
+import shutil
 
 root_dir = join(dirname(realpath(__file__)),'test')
-save_path = '' #you must indicate here the path where you want to download the file
 
 def get_file_names(directory_path):
     return [f for f in listdir(directory_path) if isfile(join(directory_path, f))]
@@ -29,7 +29,7 @@ def show_files(request):
 def delete(request):
     namefolder = request.GET.get("foldername", "")
     try:
-        rmdir(root_dir + request.path[:-7] + namefolder)
+        shutil.rmtree(root_dir + request.path[:-7] + namefolder)
     except OSError:
         return HttpResponse("<b>Delete directory failed</b>")
     else:
@@ -52,15 +52,3 @@ def download(request): #boot from server
     response = HttpResponse(file,content_type='application/msword')
     response['Content-Disposition'] = 'attachment; filename=' + namefile
     return response
-
-def upload(request): #upload to server
-    if request.method == 'POST':
-        if save_path == '':
-            return HttpResponse("<b>Specify the path where you want to download the file</b>")
-        else:
-            with open(save_path + request.FILES['document'].name, 'wb+') as destination:
-                for chunk in request.FILES['document'].chunks():
-                    destination.write(chunk)
-    return HttpResponse("<b>Success</b>")
-    if request.method == 'GET':
-        return render(request,'upload.html')
